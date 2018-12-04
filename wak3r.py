42c23de86f4305a2f9aa8871aa46303df5358248
Arsh Chauhan <achauhan@alaska.edu>
2018-12-04 12:41:51 -0900

    Add route to get all times

--- wak3r.py
+++ wak3r.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.5
 
 from flask import Flask, render_template, redirect, url_for, request, jsonify, make_response
 #from flask_pymongo import PyMongo
 import json
+from api import format_data, get_database
 
 
 app = Flask(__name__)
@@ -18,11 +19,13 @@ def index():
 		hrs = request.form['hours']
 		mins = request.form['minutes']
 		print("data:", days, hrs, mins)
-
+		print(format_data(days=days,hour=hrs, minute=mins))
 		return render_template('index.html')
 	elif request.method == 'GET':
 		return render_template('index.html')
 
-
+@app.route('/get_times', methods = ['GET'])
+def get_times():
+	return get_database(db_file='db.json')
 if __name__ == '__main__':
 	app.run()
