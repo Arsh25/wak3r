cebdd2632f4a016be946c2d3d0bda133a37923a4
amitchell <amitchell@alaska.edu>
2018-12-05 21:05:40 -0900

    functionality for multiple wake settings implemented

--- static/js/app.js
+++ static/js/app.js
@@ -57,3 +57,42 @@ $('#submit').click(function(e)
         $('#save').fadeOut('slow');
     }, 750);
 });
+
+
+/* SETTING MANIPULATION */
+
+function del(obj)
+{
+    var week = [
+        obj.parentNode.parentNode.previousElementSibling.children[0].children[0].children[0].children[0].children[1].attributes.style.value,
+        obj.parentNode.parentNode.previousElementSibling.children[0].children[0].children[0].children[1].children[1].attributes.style.value,
+        obj.parentNode.parentNode.previousElementSibling.children[0].children[0].children[0].children[2].children[1].attributes.style.value,
+        obj.parentNode.parentNode.previousElementSibling.children[0].children[0].children[0].children[3].children[1].attributes.style.value,
+        obj.parentNode.parentNode.previousElementSibling.children[0].children[0].children[0].children[4].children[1].attributes.style.value,
+        obj.parentNode.parentNode.previousElementSibling.children[0].children[0].children[0].children[5].children[1].attributes.style.value,
+        obj.parentNode.parentNode.previousElementSibling.children[0].children[0].children[0].children[6].children[1].attributes.style.value
+    ];
+
+    var days = [];
+    for (i = 0; i < week.length; ++i)
+    {
+        if (week[i] == "border-color: #f26816; color: #f26816;")
+        { days.push(String(i)); }
+    }
+    
+    var clock = obj.parentNode.parentNode.previousElementSibling.children[2].innerText;
+    var time = clock.split(":");
+
+    var seek = {'purpose': 'del', 'data': {'days': days, 'hrs': time[0], 'mins': time[1]}};
+    var req = new XMLHttpRequest();
+
+    req.open('POST', '/', true);
+
+    req.onreadystatechange = function()
+    {
+        if(req.readyState == 4 && req.status == 200)
+        { location.reload(); }
+    }
+
+    req.send(JSON.stringify(seek));
+}
--- wak3r.py
+++ wak3r.py
@@ -1,31 +1,77 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3
 
 from flask import Flask, render_template, redirect, url_for, request, jsonify, make_response
-#from flask_pymongo import PyMongo
-import json
+from flask_pymongo import PyMongo
 from api import format_data, get_database
+import json
 
 
 app = Flask(__name__)
-#app.config['MONGO_DBNAME'] = 'temps'
-#mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/wakeme" #python3
+#app.config['MONGO_DBNAME'] = 'wakeme' #python2
+mongo = PyMongo(app)
 
 
+# where the magic happens
 @app.route('/', methods = ['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		days = request.form.getlist('day')
+		if request.data:
+			seek = json.loads(request.data)
+			if seek['purpose'] == 'del':
+				del_db({"days" : seek['data']['days'], "hrs" : seek['data']['hrs'], "mins" : seek['data']['mins']})
+				print(get_db())
+		else:
+			days = request.form.getlist('day')
+			hrs = request.form['hours']
+			mins = request.form['minutes']
 
-		hrs = request.form['hours']
-		mins = request.form['minutes']
-		print("data:", days, hrs, mins)
-		print(format_data(days=days,hour=hrs, minute=mins))
-		return render_template('index.html')
+			if mins == "0" or mins == "5":
+				mins = "0" + mins
+
+			set_db(days, hrs, mins)
+
+		return redirect(url_for('index'))
 	elif request.method == 'GET':
-		return render_template('index.html')
+		return render_template('index.html', settings = get_db())
 
+
+# add a setting to the db
+def set_db(days, hrs, mins):
+	mongo.db.current.insert({'days' : days, 'hrs' : hrs, 'mins' : mins})
+
+
+# get every setting we've stored
+def get_db():
+	current = mongo.db.current.find({})
+	cur_list = []
+
+	for c in current:
+		cur_list.append( {'days' : c['days'], 'hrs' : c['hrs'], 'mins' : c['mins']} )
+
+	return cur_list
+
+
+# delete an item from the database
+def del_db(to_rm):
+	mongo.db.current.remove({"days" : to_rm['days'], "hrs" : to_rm['hrs'], "mins" : to_rm['mins']})
+
+
+# secret page, shh...
 @app.route('/get_times', methods = ['GET'])
 def get_times():
-	return get_database(db_file='db.json')
+	data = get_db()
+
+	clock = []
+
+	for i in range(len(data)):
+		days = data[i]['days']
+		time = data[i]['hrs'] + ":" + data[0]['mins']
+		clock.append({"days" : days, "time" : time})
+
+	return jsonify(clock)
+	#return get_database(db_file='db.json')
+
+
 if __name__ == '__main__':
 	app.run()
